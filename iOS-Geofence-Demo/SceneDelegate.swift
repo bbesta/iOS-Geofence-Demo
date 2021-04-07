077ca2a759f8e06210c8adf41bfdf12f2e96ecd6
bbesta <bbesta@MSysTechnologies.com>
2021-04-07 21:59:00 +0530

    Location Event update
    extend the method updated

--- iOS-Geofence-Demo/SceneDelegate.swift
+++ iOS-Geofence-Demo/SceneDelegate.swift
@@ -49,44 +49,57 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
-    func locationManager(
-      _ manager: CLLocationManager,
-      didExitRegion region: CLRegion
-    ) {
-      if region is CLCircularRegion {
-        handleEvent(for: region)
-      }
-    }
-    func handleEvent(for region: CLRegion) {
-      // Show an alert if application is active
-      if UIApplication.shared.applicationState == .active {
-        guard let message = note(from: region.identifier) else { return }
-        window?.rootViewController?.showAlert(withTitle: nil, message: message)
-      } else {
-        // Otherwise present a local notification
-        guard let body = note(from: region.identifier) else { return }
-        let notificationContent = UNMutableNotificationContent()
-        notificationContent.body = body
-        notificationContent.sound = .default
-        notificationContent.badge = UIApplication.shared.applicationIconBadgeNumber + 1 as NSNumber
-        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
-        let request = UNNotificationRequest(
-          identifier: "location_change",
-          content: notificationContent,
-          trigger: trigger)
-        UNUserNotificationCenter.current().add(request) { error in
-          if let error = error {
-            print("Error: \(error)")
-          }
-        }
-      }
-    }
-
-    func note(from identifier: String) -> String? {
-      let geofences = Geofence.allGeofences()
-      let matched = geofences.first { $0.identifier == identifier }
-      return matched?.note
-    }
+    
 
 }
+// MARK: - Location Manager Delegate
 
+extension SceneDelegate: CLLocationManagerDelegate {
+  func locationManager(
+    _ manager: CLLocationManager,
+    didEnterRegion region: CLRegion
+  ) {
+    if region is CLCircularRegion {
+      handleEvent(for: region)
+    }
+  }
+
+  func locationManager(
+    _ manager: CLLocationManager,
+    didExitRegion region: CLRegion
+  ) {
+    if region is CLCircularRegion {
+      handleEvent(for: region)
+    }
+  }
+func handleEvent(for region: CLRegion) {
+  // Show an alert if application is active
+  if UIApplication.shared.applicationState == .active {
+    guard let message = note(from: region.identifier) else { return }
+    window?.rootViewController?.showAlert(withTitle: nil, message: message)
+  } else {
+    // Otherwise present a local notification
+    guard let body = note(from: region.identifier) else { return }
+    let notificationContent = UNMutableNotificationContent()
+    notificationContent.body = body
+    notificationContent.sound = .default
+    notificationContent.badge = UIApplication.shared.applicationIconBadgeNumber + 1 as NSNumber
+    let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
+    let request = UNNotificationRequest(
+      identifier: "location_change",
+      content: notificationContent,
+      trigger: trigger)
+    UNUserNotificationCenter.current().add(request) { error in
+      if let error = error {
+        print("Error: \(error)")
+      }
+    }
+  }
+}
+
+func note(from identifier: String) -> String? {
+  let geofences = Geofence.allGeofences()
+  let matched = geofences.first { $0.identifier == identifier }
+  return matched?.note
+}
+}
